238bcadd4f0e06017a1037d2108f35af1bbca641
Klimov-V <klimov_vitaliy@icloud.com>
2020-07-06 23:21:50 +0300

    finished this work

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,35 @@ var textarea = new Vue({
 let button = new Vue({
     el: '#button',
     data: {
-
+        showBlock: false,
+        showFstInpt: false,
+        showScndInpt: false,
+        btn: '',
+        hours: 0,
+        minutes: 0,
+        seconds: '',
     },
     methods: {
         action: function() {
-            
+            this.showBlock = true;
+        },
+        checkBtn: function(e) {
+            if (e.button == 0) {
+                this.btn = 'левая кнопка мыши';
+            } else if (e.button == 1) {
+                this.btn = 'средняя кнопка мыши';
+            } else {
+                this.btn = 'правая кнопка мыши';
+            }
+            this.showFstInpt = true;
+
+        },
+        showTime: function() {
+            this.showScndInpt = true;
+            let time = new Date();
+            this.hours = time.getHours();
+            this.minutes = time.getMinutes();
+            this.seconds = time.getSeconds();
         }
     }
 });
